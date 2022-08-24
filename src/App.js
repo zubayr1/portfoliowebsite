0459e22e66f1d4fa3ab54affe0547d078cd7a717
zubayr1 <write2zubayr@gmail.com>
2022-08-24 03:47:52 +0200

    header router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import Header from './components/Header.js';
+import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
+import Home from './components/Home.js';
+import Token from './components/Token';
 
 function App() {
   return (
     <div style={{marginTop:'1%'}}>
-      <Header />
+      <Router>
+        <Routes >
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/token" element={<Token/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,33 @@ function Header() {
   return (
     <div style={{marginLeft:'2%', marginRight:'2%'}}>
       
-      <Grid columns='equal' verticalAlign="middle" >
-        <Grid.Column floated='left'>
+      <Grid  verticalAlign="middle" >
+
+        <Grid.Column mobile={6} tablet={4} computer={4} floated='left'>
           <Image src={logo} size='small' />
         </Grid.Column>
 
-        <Grid.Column >
-          <Image src={logo} size='small' />
+        <Grid.Column mobile={10} tablet={12} computer={10}>
+          <Grid columns='equal'>
+            <Grid.Column>
+              Home
+            </Grid.Column>
+
+            <Grid.Column>
+              Skills
+            </Grid.Column>
+
+            <Grid.Column>
+              Feedback
+            </Grid.Column>
+
+            <Grid.Column>
+              Token
+            </Grid.Column>
+
+          </Grid>
         </Grid.Column>
 
-        <Grid.Column floated='right'>
-                    
-        </Grid.Column>
-
-        
       </Grid>
     </div>
   )
