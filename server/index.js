20c8bd5bec7ea279ed1751e606a2fef4cd445adb
zubayr1 <write2zubayr@gmail.com>
2022-09-17 15:39:52 +0200

    add mongoose

--- server/index.js
+++ server/index.js
@@ -2,10 +2,25 @@
 require('dotenv').config()
 
 const express = require("express");
+const cors = require('cors');
+const mongoose =  require('mongoose');
 
-const PORT = process.env.SERVERPORT;
+
+const PORT = process.env.SERVERPORT || 5000;
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', ()=>
+{
+  console.log('MongoDB database connected successfully.');
+})
+
+
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -17,10 +32,17 @@ app.get("/", (req, res) => {
 });
 
 app.get("/apis", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello from zakeKal!" });
     
-  });
-  
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+});
+
+const feedbacksRouter = require('./routes/feedbacks');
+const usersrouter = require('./routes/users');
+
+app.use('/feedbacks', feedbacksRouter);
+app.use('/users', usersrouter);
+
+//listening to port...
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
