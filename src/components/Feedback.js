5e9deea4d16830f5c2ec679ec19f99af49578641
zubayr1 <write2zubayr@gmail.com>
2022-10-25 18:21:10 +0200

    add register form

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header.js';
 import { useNavigate } from 'react-router-dom';
+import {Grid, Button, Form, Segment} from 'semantic-ui-react';
 
 function Feedback() {
     const navigate = useNavigate();
@@ -15,10 +16,35 @@ function Feedback() {
         navigate("/token");  
       }
 
+    let formdisplay
+
+    formdisplay = 
+    <div style={{marginTop:'2%', marginLeft: "5%", marginRight:"5%"}}>
+      <Segment>
+      <Grid centered>
+        <Grid.Column>
+            <Form>
+              <Form.Field>
+                <label>First Name</label>
+                <input placeholder='First Name' />
+              </Form.Field>
+              <Form.Field>
+                <label>Last Name</label>
+                <input placeholder='Last Name' />
+              </Form.Field>
+              
+              <Button type='submit'>Register with Firebase</Button>
+            </Form>
+        </Grid.Column>
+      </Grid>
+      </Segment>
+    </div>
+
   return (
     <div>
         <Header changeState={changeState}/>
-        feedback
+        
+        {formdisplay}
     </div>
   )
 }
